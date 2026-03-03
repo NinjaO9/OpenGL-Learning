8590aed481e0cbfb54d3df46bb7e97233228ba42
David <davidmontesvilanova@gmail.com>
2026-03-02 19:01:18 -0800

    All initial exercises

--- main.cpp
+++ main.cpp
@@ -23,21 +23,45 @@ int main(void)
 		"{\n"
 		" FragColor = vec4(1.0f, 0.2f, 0.2f, 1.0f);"
 		"}\0";
+	const char* yellowFragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		" FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);"
+		"}\0";
 
 	float verticies[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.2f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		0.2f, -0.5f, 0.0f,
+	};
+
+	float verts[] = {
+		0.2f, -0.5f, 0.0f,
+		0.4f, 0.5f, 0.0f,
+		0.6f, -0.5f, 0.0f,
+	};
+
+	unsigned int indicies[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
 
 	unsigned int VBO;
+
+	unsigned int VBO2;
 	
 	unsigned int VAO;
+
+	unsigned int VAO2;
+
+	unsigned int EBO;
 
 	unsigned int vertexShader;
 	unsigned int fragmentShader;
 
 	unsigned int shaderProgram;
+	unsigned int yellowShaderProgram;
 
 
 	int success;
@@ -70,12 +94,30 @@ int main(void)
 
 	glGenVertexArrays(1, &VAO);
 
+	glGenVertexArrays(1, &VAO2);
+	
+	glGenBuffers(1, &EBO);
+
 	glGenBuffers(1, &VBO);
+
+	glGenBuffers(1, &VBO2);
+
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+	glBindVertexArray(VAO2);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -105,6 +147,13 @@ int main(void)
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << "Failed to compile fragment shaders\n" << infoLog << endl;
+	}
+
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success)
 	{
@@ -112,15 +161,28 @@ int main(void)
 		cout << "Failed to link shader program\n" << infoLog << endl;
 	}
 
+	glDeleteShader(fragmentShader);
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &yellowFragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << "Failed to compile fragment shaders\n" << infoLog << endl;
+	}
+
+	yellowShaderProgram = glCreateProgram();
+
+	glAttachShader(yellowShaderProgram, vertexShader);
+	glAttachShader(yellowShaderProgram, fragmentShader);
+	glLinkProgram(yellowShaderProgram);
+
+
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-
-
-	
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -128,6 +190,13 @@ int main(void)
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glUseProgram(yellowShaderProgram);
+		glBindVertexArray(VAO2);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -148,4 +217,13 @@ void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // wireframe
+	}
+	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+	{
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
 }
+
